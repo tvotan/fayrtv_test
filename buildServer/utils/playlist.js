0386da1fbc910215a7c91b0cc8f69e8adb3ca6f1
tvotan <tin.votan@theblackswan.ai>
2020-11-05 16:00:43 +0100

    upload deployment state

--- buildServer/utils/playlist.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.findPlaylistVideoByUrl = void 0;
-exports.findPlaylistVideoByUrl = function (playlist, url) {
-    if (!url)
-        return;
-    return playlist.find(function (video) { return video.url === url; });
-};
